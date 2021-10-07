b81b329db2bf57902a1266c3a72b55daaad6f7c9
Funmilayo_Aina <Funmilayomaryaina@yahoo.com>
2021-10-07 11:40:09 +0200

    Front & Backend added to repo

---
+++ eRecycle/eRecycle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eRecycle.MvcApplication" Language="C#" %>
---
+++ eRecycle/eRecycle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eRecycle.Startup))]
+namespace eRecycle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
